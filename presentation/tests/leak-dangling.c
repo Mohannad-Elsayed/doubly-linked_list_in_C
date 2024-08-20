d80e21ead57d2da0aeb6c7f0b3580c8166526fbc
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-08-20 13:13:01 +0300

    Make test in one file

--- presentation/tests/leak-dangling.c
+++ presentation/tests/leak-dangling.c
@@ -2,7 +2,6 @@
 int main(){
     int *p = malloc(4);
     *p = 4;
-    free(p);
-    free(p);
+
     return 0;
 }
--- presentation/tests/leak.c
+++
@@ -1,8 +0,0 @@
-#include <malloc.h>
-int main(){
-    int *p = malloc(4);
-    *p = 4;
-    free(p);
-    free(p);
-    return 0;
-}
